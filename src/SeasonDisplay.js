55b292b60648591b992d81d736c6583c8c473f4a
Dani Sestan <daniasestan@gmail.com>
2019-10-03 18:32:22 -0600

    based on latitude value of user's geolocation, and the month value of the current date, app will render message and icon indicating weather

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,1 +1,38 @@
 import React from 'react';
+import './SeasonDisplay.css';
+
+class SeasonDisplay extends React.Component {
+    state = {
+        weather: {
+            id: '',
+            message: '',
+            icon: '',
+        }
+    };
+
+    componentDidMount() {
+        var month = new Date().getMonth();
+        if (month >= 3 && month <= 8) {
+            if (this.props.lat > 0)
+                this.setState({weather: {id: 'warm', message:"It's warm out there!", icon: 'sun'}});
+            else
+                this.setState({weather: {id: 'cold', message:"It's cold out there!", icon: 'snowflake'}});
+        }else {
+            if (this.props.lat > 0)
+                this.setState({weather: {id: 'cold', message:"It's cold out there!", icon: 'snowflake'}});
+            else
+                this.setState({weather: {id: 'warm', message:"It's warm out there!", icon: 'sun'}});
+        }
+    }
+    render(){
+        return(
+            <div className={`${this.state.weather['id']}`}>
+                <i className={`icon-left massive ${this.state.weather['icon']} icon`}  />
+                <h1 className='season-display'>{this.state.weather['message']}</h1>
+                <i className={`icon-right massive ${this.state.weather['icon']} icon`}  />
+            </div>
+        )
+    }
+}
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//
-// const App = () => {
-//   window.navigator.geolocation.getCurrentPosition(
-//       (position) => console.log(position),
-//       (err) => console.log(err)
-//   );
-//   return <div>Latitude</div>
-// };
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from "./Spinner";
+import './SeasonDisplay.css';
+
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-  render(){
-    return <div>Latitude: </div>
+  state = {
+    lat: null,
+    errorMessage: ''
   };
+
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => this.setState({lat: position.coords.latitude}),
+        (err) => this.setState({errorMessage: err.message})
+    );
+  }
+
+  renderContent(){
+    if(this.state.errorMessage && !this.state.lat){
+      return (
+          <div>
+            Error: {this.state.errorMessage}
+          </div>
+      )
+    } else if(!this.state.errorMessage && this.state.lat){
+      return (
+          <div>
+            {/*Lat: {this.state.lat}*/}
+            <SeasonDisplay lat = {this.state.lat}/>
+          </div>
+      )
+    } else {
+      return <Spinner message="Please accept request for location."/>;
+    }
+  };
+
+  render() {
+    return <div className='weather'>{this.renderContent()}</div>
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
